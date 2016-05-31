9f207a2c7268e42999a3c2ea44f20be67927abf3
IdioticMadman <704124169@qq.com>
2016-05-31 22:21:50 +0800

    Change the code style

--- app/src/main/java/gdg/androidtitlan/dagger2_demo/callback/Callback.java
+++ app/src/main/java/gdg/androidtitlan/dagger2_demo/callback/Callback.java
@@ -1,25 +1,17 @@
 /**
- * Copyright 2015 Erik Jhordan Rey.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2015 Erik Jhordan Rey. <p/> Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at <p/> http://www.apache.org/licenses/LICENSE-2.0 <p/> Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
  */
 
 package gdg.androidtitlan.dagger2_demo.callback;
 
+import gdg.androidtitlan.dagger2_demo.mvp.model.Category;
 import java.util.List;
 
-import gdg.androidtitlan.dagger2_demo.mvp.model.Category;
-
 public interface Callback {
-    void onLoadCategories(List<Category> categories);
+  void onLoadCategories(List<Category> categories);
 }
--- app/src/main/java/gdg/androidtitlan/dagger2_demo/mvp/presenter/impl/CategoryPresenterImpl.java
+++ app/src/main/java/gdg/androidtitlan/dagger2_demo/mvp/presenter/impl/CategoryPresenterImpl.java
@@ -1,53 +1,43 @@
 /**
- * Copyright 2015 Erik Jhordan Rey.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2015 Erik Jhordan Rey. <p/> Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at <p/> http://www.apache.org/licenses/LICENSE-2.0 <p/> Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
  */
 
 package gdg.androidtitlan.dagger2_demo.mvp.presenter.impl;
 
-import java.util.List;
-
 import gdg.androidtitlan.dagger2_demo.callback.Callback;
-import gdg.androidtitlan.dagger2_demo.mvp.ui.CategoryView;
 import gdg.androidtitlan.dagger2_demo.interactors.CategoryInteractor;
 import gdg.androidtitlan.dagger2_demo.mvp.model.Category;
 import gdg.androidtitlan.dagger2_demo.mvp.presenter.CategoryPresenter;
+import gdg.androidtitlan.dagger2_demo.mvp.ui.CategoryView;
+import java.util.List;
 
 public class CategoryPresenterImpl implements CategoryPresenter, Callback {
 
-    private CategoryView mainView;
-    private CategoryInteractor categoryInteractor;
+  private CategoryView categoryView;
+  private CategoryInteractor categoryInteractor;
 
-    public CategoryPresenterImpl(CategoryView mainView, CategoryInteractor categoryInteractor) {
-        this.mainView = mainView;
-        this.categoryInteractor = categoryInteractor;
-    }
+  public CategoryPresenterImpl(CategoryView categoryView, CategoryInteractor categoryInteractor) {
+    this.categoryView = categoryView;
+    this.categoryInteractor = categoryInteractor;
+  }
 
-    @Override
-    public void onResume() {
-        mainView.showProgress();
-        categoryInteractor.loadCategories(this);
-    }
+  @Override public void onResume() {
+    categoryView.showProgress();
+    categoryInteractor.loadCategories(this);
+  }
 
-    @Override
-    public void onItemSelected(Category category, int position) {
-        mainView.showMessage(String.format(category.getName() + " ->" + " Position %d clicked", position));
-    }
+  @Override public void onItemSelected(Category category, int position) {
+    categoryView.showMessage(
+        String.format(category.getName() + " ->" + " Position %d clicked", position));
+  }
 
-    @Override
-    public void onLoadCategories(List<Category> categories) {
-        mainView.showCategories(categories);
-        mainView.hideProgress();
-    }
+  @Override public void onLoadCategories(List<Category> categories) {
+    categoryView.showCategories(categories);
+    categoryView.hideProgress();
+  }
 }
